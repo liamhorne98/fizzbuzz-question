11499d867b51910ab0f30ba46a95c596ac390f7b
liamhorne98 <71072743+liamhorne98@users.noreply.github.com>
2020-11-13 13:45:44 +0200

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for x in range(0,100):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+        continue
+    elif x % 3 == 0:
+        print("Fizz")
+        continue
+    elif x % 5 == 0:
+        print("Buzz")
+        continue
+    print(x)
